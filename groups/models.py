d41dd951ab8fe84c529d9bac3d6d0f53bea5ab7f
davidfernandez999 <david.fernandez99999@gmail.com>
2023-11-24 21:04:18 -0500

    third

--- groups/models.py
+++ groups/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 # permite hacer mas legible el direccionamiento web
 from django.utils.text import slugify
+import misaka
+# para direccionamiento
+from django.urls import reverse
+# el metodo get_user_model identifica al usuario que esta registrado y lo conecta a la clase User
 from django.contrib.auth import get_user_model
 User = get_user_model  # nopep8
+# todavia no entiendo la importacion del template
 from django import template  # nopep8
 register = template.Library()  # nopep8
 
@@ -19,17 +24,27 @@ class Group(models.Model):
     slug = models.SlugField(allow_unicode=True, unique=True)
     # definimos una descripción
     description = models.TextField(max_length=500, blank=True, default='')
-    #
+    # descripcion del markdown text
     description_html = models.TextField(editable=False, default='', blank=True)
-    #
+    # definimos todos los miembros que pertenecen a este grupo
     members = models.ManyToManyField(User, through='GroupMember')
 
     def __str__(self) -> str:
         return self.name
 
-    # hace el slug al nombre cuando se guarda la instancia
     def save(self, *args, **kwargs):
+        '''funcion que mejora la visualidad en la direccion del navegador'''
         self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        '''funcion por defecto para cuando termine de crear un post vaya a una url determinada, se sustituye la PrimaryKey por el slug'''
+        return reverse("groups:single", kwargs={"slug": self.slug})
+
+    class Meta:
+        # definimos el orden en que queremos ver los grupos
+        ordering = ['name']
 
 
 # clase que identifica a los miembros de un grupo
@@ -46,4 +61,5 @@ class GroupMember(models.Model):
         return self.user.username
 
     class Meta:
+        # definimos una pareja de elementos que juntos deben ser unicos, por ejemplo en cada grupo no pueden haber dos usuarios con el mismo nombre
         unique_together = ('group', 'user')
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,38 @@
+from collections.abc import Iterable
 from django.db import models
+from django.conf import settings
+# para direccionamiento
+from django.urls import reverse
+import misaka
+from groups.models import Group
+from django.contrib.auth import get_user_model
+User = get_user_model  # nopep8
+
 
 # Create your models here.
+# POST MODELS
+
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name='posts')
+    # el auto_now=True permite que el sistema ponga la fecha y la hora automaticamente en el post
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(
+        Group, related_name='posts', null=True,  blank=True)
+
+    def __str__(self) -> str:
+        return self.message
+
+    def save(self, *args, **kwargs):
+        self.message_html = misaka.html(self.message)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("post:single", kwargs={'username': self.user.username, "pk": self.pk})
+
+    class Meta:
+        # definimos el orden en el que queremos ver los posts
+        ordering = ['-created_at']
+        # definimos una pareja de elementos que juntos deben ser unicos, por ejemplo a cada mensaje le corresponde un solo usuario
+        unique_together = ['user', 'message']
